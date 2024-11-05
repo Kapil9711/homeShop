abed2c2a474d62a9efdffe3890796c0698934d43
kapil <kapil.personal9711@gmail.com>
2024-11-05 11:45:03 +0530

    added user and product model and controler for product

---
+++ server/controllers/productController.js
@@ -0,0 +1,18 @@
+import catchAsyncError from "../middlewares/catchAsyncError.js";
+import Product from "../models/product.js";
+import CustomError from "../utils/customError.js";
+
+// getAllProducts = /api/products ->get
+export const getAllProducts = catchAsyncError(async (req, res, next) => {
+  const products = await Product.find({});
+  res.status(200).json(products);
+});
+
+//getSingleProduct = /api/products/:id
+export const getSingleProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new CustomError("Product not found", 404));
+  }
+  res.status(200).json(product);
+});
--- server/server.js
+++ server/server.js
@@ -1,22 +1,47 @@
 import express from "express";
-const app = express();
 import connectDb from "./config/db.js";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import hpp from "hpp";
+import mongoSatitize from "express-mongo-sanitize";
+import session from "express-session";
+import productRouter from "./router/productRouter.js";
+import cors from "cors";
+import globalErrorHandler from "./middlewares/errorMiddleware.js";
+const app = express();
 dotenv.config();
+app.use(cors());
 
+// setup security headers
+app.use(helmet());
 // body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// endpoints
+// sanitize data for noSql atack
+app.use(mongoSatitize());
+// prevent parameter pollution
+app.use(hpp());
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+// Middleware for session management
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // Replace with your session secret
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set to true if using HTTPS
+  })
+);
+
+// endpoints
+app.use("/api/products", productRouter);
+
+// global error handler
+app.use(globalErrorHandler);
 
 // spining server
 const PORT = process.env.PORT || 8000;
-const MODE = process.env.MODE || "production";
+const MODE = process.env.NODE_ENV || "production";
 
 const start = async () => {
   try {
